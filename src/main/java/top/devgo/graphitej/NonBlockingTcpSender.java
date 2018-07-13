538582bc6c4ada96d2b1e3e358d617d6c21e0a3a
dpy1123 <dpy1123@126.com>
2018-07-13 14:58:59 +0800

    auto reconnect

--- src/main/java/top/devgo/graphitej/NonBlockingTcpSender.java
+++ src/main/java/top/devgo/graphitej/NonBlockingTcpSender.java
@@ -5,24 +5,29 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+
 
 final class NonBlockingTcpSender {
+    private final String hostname;
+    private final int port;
     private final Charset encoding;
-    private final SocketChannel clientSocket;
+    private SocketChannel clientSocket;
     private final ExecutorService executor;
+    private final ScheduledThreadPoolExecutor timer;
     private GraphiteJErrorHandler handler;
 
     NonBlockingTcpSender(String hostname, int port, Charset encoding, GraphiteJErrorHandler handler) throws IOException {
+        this.hostname = hostname;
+        this.port = port;
         this.encoding = encoding;
         this.handler = handler;
-        this.clientSocket = SocketChannel.open();
-        this.clientSocket.connect(new InetSocketAddress(hostname, port));
 
-        this.executor = Executors.newSingleThreadExecutor(new ThreadFactory() {
+        // blocking sender
+        clientSocket = SocketChannel.open();
+        clientSocket.connect(new InetSocketAddress(hostname, port));
+
+        executor = Executors.newSingleThreadExecutor(new ThreadFactory() {
             final ThreadFactory delegate = Executors.defaultThreadFactory();
             @Override public Thread newThread(Runnable r) {
                 Thread result = delegate.newThread(r);
@@ -31,6 +36,8 @@ final class NonBlockingTcpSender {
                 return result;
             }
         });
+
+        timer = new ScheduledThreadPoolExecutor(1);
     }
 
     void stop() {
@@ -55,17 +62,41 @@ final class NonBlockingTcpSender {
 
     void send(final String message) {
         try {
-            executor.execute(() -> socketSend(message));
+            executor.execute(() -> blockingSend(message));
         }
         catch (Exception e) {
             handler.handle(e);
         }
     }
 
-    private void socketSend(String message) {
+    private volatile boolean reconnecting = false;
+    private void reconnect() {
+        if (!reconnecting){
+            reconnecting = true;
+            doReconnect();
+        }
+    }
+    private void doReconnect() {
+        timer.schedule(() -> {
+            try {
+                clientSocket.close();
+                clientSocket = SocketChannel.open();
+                clientSocket.connect(new InetSocketAddress(hostname, port));
+                reconnecting = false;
+            } catch (IOException e) {
+                doReconnect();
+            }
+        }, 5, TimeUnit.SECONDS);
+    }
+
+    private void blockingSend(String message) {
         try {
             final byte[] sendData = message.getBytes(encoding);
             clientSocket.write(ByteBuffer.wrap(sendData));
+        } catch (IOException e) {
+            if ("Broken pipe".equals(e.getMessage())){
+                reconnect();
+            }
         } catch (Exception e) {
             handler.handle(e);
         }
